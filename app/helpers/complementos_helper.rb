e7e4f0a220899b84bdc828d3b6a4d6cb166a98d0
mgtsuzuki <64231260+mgtsuzuki@users.noreply.github.com>
2020-12-02 09:48:48 -0300

    O formulário de complementos foi adaptado.

--- app/helpers/complementos_helper.rb
+++ app/helpers/complementos_helper.rb
@@ -1,2 +1,32 @@
 module ComplementosHelper
+    def buttons(model_name, target_property, button_source)
+        html=''
+        list = button_source.sort
+        html << '<div  class="custom-control  custom-radio"><legend>Pais</legend>'
+        list.each do |x|
+            html << radio_button(model_name, target_property, x[1])
+            html << "  " + (x[0])
+            html << '<br>'
+        end
+        html << '</div>'
+        return html.html_safe
+    end
+
+    def button_select(model_name, target_property, button_source)
+        html=''
+        list = button_source.sort
+        if list.length < 4
+            html << '<div  class="custom-control custom-radio"><legend>Pais</legend>'
+            list.each do |x|
+                html << radio_button(model_name, target_property, x[1])
+                html << "  " + (x[0])
+                html << '<br>'
+            end
+            html << '</div>'
+        else
+            html << ' <label for="complement_pais">Pais</label><br>'
+            html << select(model_name, target_property, list)
+        end
+        return html.html_safe
+    end
 end
